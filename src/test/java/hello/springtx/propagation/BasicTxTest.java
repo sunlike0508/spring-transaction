1d43681e775895a2fc458228578cb99ce4886e1c
seonhoshin <sunlike0508@gmail.com>
2024-10-05 12:56:39 +0900

    전파 끝

--- src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -4,12 +4,14 @@ package hello.springtx.propagation;
 import javax.sql.DataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestReporter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
 
@@ -121,6 +123,47 @@ public class BasicTxTest {
 
         log.info("외부 트랜잭션 commit");
         transactionManager.commit(outer);
+    }
+
+    @Test
+    void commit7() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = this.transactionManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.inNew() = {}", outer.isNewTransaction());
+
+        log.info("내부 트랜잭션 시작");
+        DefaultTransactionAttribute defaultTransactionAttribute= new DefaultTransactionAttribute();
+        defaultTransactionAttribute.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+
+        TransactionStatus inner = transactionManager.getTransaction(defaultTransactionAttribute);
+        log.info("inner.inNew() = {}", inner.isNewTransaction());
+
+        log.info("내부 트랜잭션 롤백");
+        transactionManager.rollback(inner);
+
+        log.info("외부 트랜잭션 커밋");
+        transactionManager.commit(outer);
+    }
+
+
+    @Test
+    void commit8() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = this.transactionManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.inNew() = {}", outer.isNewTransaction());
+
+        log.info("내부 트랜잭션 시작");
+        DefaultTransactionAttribute defaultTransactionAttribute= new DefaultTransactionAttribute();
+        defaultTransactionAttribute.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+
+        TransactionStatus inner = transactionManager.getTransaction(defaultTransactionAttribute);
+        log.info("inner.inNew() = {}", inner.isNewTransaction());
+
+        log.info("내부 트랜잭션 커밋");
+        transactionManager.commit(inner);
+
+        log.info("외부 트랜잭션 롤백");
+        transactionManager.rollback(outer);
     }
 }
 
